40f5049c1a4a27d9beb6656dfb0ff02f8cf0edd0
Victor Mendes Reis <61296590+Viictorreiss@users.noreply.github.com>
2021-05-21 18:39:52 -0300

    app_envio

---
+++ AppEnvio/app/src/main/java/br/usjt/sistemasdistribuidos/appenvio/MainActivity.java
@@ -0,0 +1,56 @@
+package br.usjt.sistemasdistribuidos.appenvio;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent intent = getIntent();
+
+        String mensagemRecebida = intent.getStringExtra(Intent.EXTRA_TEXT);
+
+        Toast.makeText(this,"Mensagem recebida: "+mensagemRecebida,Toast.LENGTH_LONG).show();
+    }
+
+    public void enviarMensagem (View v){
+//referência para o campo em que o usuário digita
+        EditText mensagemEditText = findViewById(R.id.mensagemEditText);
+//mensagem digitada pelo usuário
+        String mensagem = mensagemEditText.getEditableText().toString();
+//intent a ser enviado para o SO, a princípio vazio
+        Intent enviarMensagemIntent = new Intent();
+//a ação desejada é "SEND", que representa compartilhar conteúdo
+        enviarMensagemIntent.setAction(Intent.ACTION_SEND);
+//tipo do conteúdo manipulado especificado como um MIME Type
+        enviarMensagemIntent.setType("text/plain");
+
+        enviarMensagemIntent.setClassName("br.usjt.sistemasdistribuidos.apprecebimento",
+                "br.usjt.sistemasdistribuidos.apprecebimento.MainActivity");
+
+
+//mensagem vai dentro do Intent, associada à constante EXTRA_TEXT
+//para que possa ser "pega" pela Activity que receberá esse Intent
+        enviarMensagemIntent.putExtra(Intent.EXTRA_TEXT, mensagem);
+//construímos um Intent que "empacota" o atual e garante
+// que a tela de escolha de app sempre apareça e que o usuário
+//não possa selecionar um app padrão
+        enviarMensagemIntent = Intent.createChooser(enviarMensagemIntent, null);
+//envia o Intent para o Android
+//qualquer Activity (até de outros apps) que, em tempo de intalação
+//tiver especificado ser capaz de atender requisições cuja ACTION seja
+//ACTION_SEND será considerada candidata pelo Android
+        startActivity(enviarMensagemIntent);
+    }
+
+
+}
